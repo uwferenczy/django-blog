59b378fe3c02e0ee939c3dbc576e01681faaac8e
uwferenczy <ferenczy@uw.edu>
2022-06-03 11:03:35 -0700

    Updated blogging and polling apps for module 8.

--- blogging/views.py
+++ blogging/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse,Http404
+from django.http.response import HttpResponse, Http404
 # from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
 
 # Create your views here.
 def stub_view(request, *args, **kwargs):
@@ -15,20 +18,32 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    #template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    #body = template.render(context)
-    #return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html',context)
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html',context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+
+
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     # template = loader.get_template('blogging/list.html')
+#     context = {'posts': posts}
+#     # body = template.render(context)
+#     # return HttpResponse(body, content_type="text/html")
+#     return render(request, 'blogging/list.html', context)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_engine = 'blogging/detail.html'
+
+    def publish(self, request, *args, **kwargs):
+        # def detail_view(request, post_id):
+        blog = self.get_object()
+        published = blog.objects.exclude(published_date__exact=None)
+        try:
+            post = published.get(pk=blog.post_id)
+        except Post.DoesNotExist:
+            raise Http404
+        context = {"posts": post}
+        return render(request, 'blogging/detail.html', context)
--- polling/urls.py
+++ polling/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from polling.views import list_view, detail_view
+from polling.views import PollDetailView, PollListView
 
 urlpatterns = [
-    path('', list_view, name="poll_index"),
-    path('polls/<int:poll_id>', detail_view, name="poll_detail")  # polling/polls/1
+    path('', PollListView.as_view(), name="poll_index"),
+    path('polls/<int:pk>',PollDetailView.as_view(), name="poll_detail" )
+    #path('', list_view, name="poll_index"),
+    #path('polls/<int:poll_id>', detail_view, name="poll_detail")  # polling/polls/1
 
 ]
